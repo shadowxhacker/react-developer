ccc96068793e8b40c14b04fd225b112c13d087c7
Taha <alet8319@gmail.com>
2024-09-17 12:18:27 +0500

    first commit

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useMemo } from 'react';
+import React, { useEffect, useState, useCallback, useMemo, useRef } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { doc, getDoc, setDoc, collection, query, onSnapshot, deleteDoc, serverTimestamp, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
@@ -8,6 +8,8 @@ import { MdOutlineDelete, MdFavoriteBorder, MdFavorite, MdImage, MdVideoLibrary,
 import { Helmet } from 'react-helmet';
 import { RotatingLines } from 'react-loader-spinner';
 import { useNavigate } from 'react-router-dom';
+import { debounce } from 'lodash';
+import imageCompression from 'browser-image-compression';
 
 
 const Blog = () => {
@@ -16,6 +18,7 @@ const Blog = () => {
     const [media, setMedia] = useState(null);
     const [mediaType, setMediaType] = useState('');
     const [posts, setPosts] = useState([]);
+    const videoRefs = useRef([]);
     const [isFormOpen, setIsFormOpen] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [currentUserId, setCurrentUserId] = useState(null);
@@ -30,7 +33,7 @@ const Blog = () => {
             if (user) {
                 setIsLoggedIn(true);
                 setCurrentUserId(user.uid);
-    
+
                 try {
                     const userRef = doc(db, 'users', user.uid);
                     const userSnap = await getDoc(userRef);
@@ -51,33 +54,58 @@ const Blog = () => {
                 setUserProfilePic('')
             }
         });
-    
+
         const q = query(collection(db, 'posts'));
         const unsubscribePosts = onSnapshot(q, (querySnapshot) => {
             const postsData = [];
             querySnapshot.forEach((doc) => {
                 postsData.push({ id: doc.id, ...doc.data() });
             });
-    
-            // Sort posts based on timestamp so that the newest posts come first
+
+            
             const sortedPosts = postsData.sort((a, b) => {
                 const aTimestamp = a.timestamp ? a.timestamp.toDate().getTime() : 0;
                 const bTimestamp = b.timestamp ? b.timestamp.toDate().getTime() : 0;
-                return bTimestamp - aTimestamp; // Newest posts first
+                return bTimestamp - aTimestamp;
             });
-    
+
             setPosts(sortedPosts);
         }, (error) => {
             console.error("Error fetching posts: ", error.message);
             toast.error('Error fetching posts.');
         });
-    
+
         return () => {
             unsubscribeAuth();
             unsubscribePosts();
         };
     }, []);
-    
+
+    const handleInputChange = useCallback(
+        debounce((value) => {
+            setTitle(value);
+        }, 300),
+        []
+    )
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    const video = entry.target;
+                    if (entry.isIntersecting) {
+                        video.play();
+                    } else {
+                        video.pause();
+                    }
+                });
+            },
+            { threshold: 0.5, rootMargin: '0px 0px  -50% 0px' }
+        );
+
+        videoRefs.current.slice(0, 10).forEach((video) => video && observer.observe(video));
+        return () => videoRefs.current.forEach((video) => video && observer.unobserve(video));
+      }, [posts]);
 
     const formatTimestamp = useCallback((timestamp) => {
         if (!timestamp) return '';
@@ -94,7 +122,7 @@ const Blog = () => {
         return `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}`;
     }, []);
 
-    const handleMediaChange = (e) => {
+    const handleMediaChange = useCallback((e) => {
         const file = e.target.files[0];
         if (file) {
             const fileType = file.type;
@@ -108,14 +136,21 @@ const Blog = () => {
                 toast.error('Only image and video files are allowed.');
                 return;
             }
-
             setMedia(file);
         }
-    };
+    }, []);
 
     const uploadMedia = async (file) => {
         const storageRef = ref(storage, `media/${file.name}`);
         const uploadTask = uploadBytesResumable(storageRef, file);
+        
+        if(file.type.startsWith('image/')){
+            const compressedFile = await imageCompression(file, {
+                maxSizeMB: 50,
+                maxWidthOrHeight: 1920,
+            });
+            file = compressedFile;
+        }
 
         return new Promise((resolve, reject) => {
             uploadTask.on(
@@ -142,27 +177,27 @@ const Blog = () => {
             toast.error('Title and description are required.');
             return;
         }
-    
+
         if (description.length > 5000) {
             toast.error('Description cannot exceed 5000 characters.');
             return;
         }
-    
+
         setLoading(true);
-    
+
         try {
             const currentUser = auth.currentUser;
-    
+
             if (!currentUser) {
                 toast.error('You must be logged in to create a post.');
                 return;
             }
-    
+
             let mediaURL = null;
             if (media) {
                 mediaURL = await uploadMedia(media);
             }
-    
+
             const newPost = {
                 title,
                 description,
@@ -175,10 +210,10 @@ const Blog = () => {
                 likes: [],
                 comment: []
             };
-    
+
             const postRef = doc(collection(db, 'posts'));
             await setDoc(postRef, newPost);
-    
+
             setTitle('');
             setDescription('');
             setMedia(null);
@@ -191,7 +226,7 @@ const Blog = () => {
             setLoading(false);
         }
     };
-    
+
 
     const deletePost = async (id, mediaURL) => {
         try {
@@ -240,17 +275,17 @@ const Blog = () => {
         try {
             const currentUserRef = doc(db, 'users', currentUserId);
             const userToFollowRef = doc(db, 'users', userId);
-    
+
             // Add userId to the current user's following list
             await updateDoc(currentUserRef, {
                 following: arrayUnion(userId)
             });
-    
+
             // Add currentUserId to the followed user's followers list
             await updateDoc(userToFollowRef, {
                 followers: arrayUnion(currentUserId)
             });
-    
+
             setFollowedUsers(prev => new Set(prev).add(userId));
             toast.success('User followed successfully!');
         } catch (error) {
@@ -258,27 +293,27 @@ const Blog = () => {
             toast.error(`Error following the user: ${error.message}`);
         }
     };
-    
+
     const unfollowUser = async (userId) => {
         if (!isLoggedIn) {
             toast.error('You must be logged in to unfollow users.');
             return;
         }
-    
+
         try {
             const currentUserRef = doc(db, 'users', currentUserId);
             const userToUnfollowRef = doc(db, 'users', userId);
-    
+
             // Remove userId from the current user's following list
             await updateDoc(currentUserRef, {
                 following: arrayRemove(userId)
             });
-    
+
             // Remove currentUserId from the unfollowed user's followers list
             await updateDoc(userToUnfollowRef, {
                 followers: arrayRemove(currentUserId)
             });
-    
+
             setFollowedUsers(prev => {
                 const newSet = new Set(prev);
                 newSet.delete(userId);
@@ -290,7 +325,7 @@ const Blog = () => {
             toast.error(`Error unfollowing the user: ${error.message}`);
         }
     };
-    
+
 
     const handleFollowButtonClick = (postUserId) => {
         if (followedUsers.has(postUserId)) {
@@ -314,7 +349,7 @@ const Blog = () => {
         </button>
     ), [isLoggedIn, isFormOpen]);
 
-    
+
     return (
         <div className='bg-[#101010] pt-2 min-h-screen flex flex-col justify-between'>
             <Helmet>
@@ -330,16 +365,16 @@ const Blog = () => {
                 <div className={`fixed inset-0 bg-[#101010] bg-opacity-50 z-50 flex items-center justify-center ${isFormOpen ? 'block' : 'hidden'}`}>
                     <div className='bg-[#171717] p-6 rounded-lg shadow-lg w-full max-w-lg'>
                         <div className='flex justify-between items-center mb-4'>
-                    <h2 className="text-xl text-gray-400 font-semibold mb-4">Create a New Post</h2>
-                    <button onClick={() => setIsFormOpen(false)} className="text-gray-500 hover:text-gray-800">
+                            <h2 className="text-xl text-gray-400 font-semibold mb-4">Create a New Post</h2>
+                            <button onClick={() => setIsFormOpen(false)} className="text-gray-500 hover:text-gray-800">
                                 <MdClose size={24} />
                             </button>
-                    </div>
+                        </div>
                         <input
                             type='text'
                             placeholder='Add Title'
                             value={title}
-                            onChange={(e) => setTitle(e.target.value)}
+                            onChange={(e) => handleInputChange(e.target.value)}
                             className='p-2 w-full mb-4 bg-transparent text-gray-300 shadow-inner outline-none'
                         />
                         <textarea
@@ -352,11 +387,11 @@ const Blog = () => {
                         <div className='relative mb-4'>
                             <label className='mr-4 cursor-pointer'>
                                 <MdImage className='text-[24px] text-gray-400' />
-                            <input type="file" accept='image/*' onChange={handleMediaChange} className="hidden" />
+                                <input type="file" accept='image/*' onChange={handleMediaChange} className="hidden" />
                             </label>
                             <label className='mr-4 cursor-pointer'>
                                 <MdVideoLibrary className='text-[24px] text-gray-400' />
-                            <input type="file" accept='video/*' onChange={handleMediaChange} className="hidden" />
+                                <input type="file" accept='video/*' onChange={handleMediaChange} className="hidden" />
                             </label>
                             <div className='w-full h-48 border-2 border-dashed border-gray-600 rounded-xl flex items-center justify-center'>
                                 {media ? (
@@ -386,7 +421,7 @@ const Blog = () => {
 
             <div className='w-full mt-5'>
                 {posts.length === 0 && <p className='text-center text-gray-500'>No posts available.</p>}
-                {posts.map((post) => (
+                {posts.map((post, index) => (
                     <div key={post.id} className='w-full max-w-lg mx-auto bg-transparent border-b-[0.1px] border-[#8c8c8c] shadow-md p-5 mb-4'>
                         <div className='flex items-center mb-3'>
                             <img
@@ -401,8 +436,8 @@ const Blog = () => {
                             </div>
                             {post.userId !== currentUserId && (
                                 <button
-                                onClick={() => handleFollowButtonClick(post.userId)}
-                                className={`ml-4 py-2 px-4 rounded ${followedUsers.has(post.userId) ? 'bg-red-500 text-white' : 'bg-gray-500 text-white'}`}
+                                    onClick={() => handleFollowButtonClick(post.userId)}
+                                    className={`ml-4 py-2 px-4 rounded ${followedUsers.has(post.userId) ? 'bg-red-500 text-white' : 'bg-gray-500 text-white'}`}
                                 >
                                     {followedUsers.has(post.userId) ? 'unfollow' : 'Follow'}
                                 </button>
@@ -411,16 +446,25 @@ const Blog = () => {
                         <h2 className='text-xl font-semibold mb-2 text-[#EAECEE]'>{post.title}</h2>
                         <p className='mb-2 text-[#EAECEE]'>{post.description}</p>
                         {post.mediaType === 'image' &&
-                        <img
-                         src={post.media} 
-                         alt='Post Media' 
-                         className='w-full h-[500px] object-cover rounded-lg mb-2'
-                         />}
-                        {post.mediaType === 'video' &&
-                            <video controls className='w-full h-[500px] object-cover rounded-lg mb-2'>
-                                <source src={post.media} />
-                                </video>
-                            }
+                            <img
+                                loading='lazy'
+                                src={post.media}
+                                alt='Post Media'
+                                className='w-full h-[500px] object-cover rounded-lg mb-2'
+                            />}
+                        {post.mediaType === 'video' && (
+                            <video
+                                id='myVideo'
+                                autoPlay
+                                muted
+                                src={post.media}
+                                preload='metadata'
+                                ref={el => (videoRefs.current[index] = el)}
+                                controls
+                                className='w-full h-[500px] object-cover rounded-lg mb-2'>
+                                <source src="video.mp4" type="video/mp4" />
+                            </video>
+                        )}
                         <div className='flex items-center justify-between'>
                             <button
                                 onClick={() => likePost(post.id, post.likes)}
